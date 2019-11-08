a2b43ea014fc19773838568571f967b9f72496c3
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-11-08 20:29:34 -0300

    completed gif list component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
-import GifList from './gif-list.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render () {
+    const gifs = [
+      { id: "WU7RukwpWyuiEBPOG4"},
+      { id: "Ze43DUajKqJjeGwW5b"},
+    ];
+
     return (
       <div>
         <div className="left-scene">
@@ -14,7 +19,7 @@ class App extends Component {
           </div>
         </div>
         <div className="right-scene">
-          <GifList />
+          <GifList gifs={gifs}/>
         </div>
       </div>
     );
